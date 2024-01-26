8fffc6342e4d73d6f34960d40e619afeb1dc5e62
yarikthree <djemzee@inbox.ru>
2024-01-26 21:20:50 +0300

    1.1. Создание объекта [#165452]

--- src/main/java/ru/job4j/tracker/oop/Dog.java
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker.oop;
 
 public class Dog {
     public static void main(String[] args) {
-        Dog polkan = new Dog();
+        Dog palkan = new Dog();
         Dog sharik = new Dog();
         Dog zhychka = new Dog();
     }
