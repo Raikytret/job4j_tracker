24694d5730b1d89d341f30f0342813104b0f02a9
yarikthree <djemzee@inbox.ru>
2024-01-28 17:15:43 +0300

    1.6. Взаимодействие объектов [#166087]

--- src/main/java/ru/job4j/tracker/oop/Ball.java
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
-        String escape = condition ? "Was eaten" : "Run away";
+        String escape = condition ? "Kilobok was eaten" : "Kolobok run away";
         System.out.println(escape);
     }
 }
