7904897a6410c0e512ff852c285e1d039c659108
yarikthree <djemzee@inbox.ru>
2024-01-28 23:11:26 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,20 +6,21 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
     public Item() {
+
     }
 
     public Item(String name) {
-        this.name =  name;
+        this.name = name;
     }
 
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
