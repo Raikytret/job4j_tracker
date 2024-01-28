27fc3efb775ae91c539f731356eb707c9965a64e
yarikthree <djemzee@inbox.ru>
2024-01-28 17:49:20 +0300

    Удаление

--- src/main/java/ru/job4j/tracker/oop/Ball.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.tracker.oop;
-
-public class Ball {
-    public void tryRun(boolean condition) {
-        String escape = condition ? "Kilobok was eaten" : "Kolobok run away";
-        System.out.println(escape);
-    }
-}
